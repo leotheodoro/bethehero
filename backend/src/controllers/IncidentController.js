934c92911e668a75e1cb3e5aff541444ff17c094
Leonardo Theodoro <2038128-theodoro.dev@users.noreply.gitlab.com>
2020-03-24 12:24:35 -0300

    Initial Commit

---
+++ backend/src/controllers/IncidentController.js
@@ -0,0 +1,47 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const offset = (page - 1) * 5;
+
+    const [count] = await connection('incidents').count();
+    const incidents = await connection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset(offset)
+      .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.whatsapp', 'ongs.uf']);
+    
+    res.header('X-Total-Count', count['count(*)']);
+    return res.json(incidents)
+  },
+
+  async store(req, res) {
+    const { title, description, value } = req.body;
+    const ong_id = req.headers.authorization;
+
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id,
+    });
+
+    return res.json({id, title, description, value});
+  },
+
+  async delete(req, res) {
+    const { id } = req.params;
+    const ong_id = req.headers.authorization;
+
+    const incident = await connection('incidents').where('id', id).select('ong_id').first();
+
+    if(incident.ong_id !== ong_id) {
+      return res.status(401).json({ error: 'Operation not allowed' });
+    }
+
+    await connection('incidents').where('id', id).delete();
+
+    return res.status(204).send();
+  }
+}
